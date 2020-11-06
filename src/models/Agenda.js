3e55d3e02cea39c972694c739ada73dae7c136ee
Henrique Silva <henrique9811@hotmail.com>
2020-11-06 17:43:21 -0300

    criação da agenda do usuário

---
+++ src/models/Agenda.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes} = require('sequelize');
+
+
+class Agenda extends Model {
+
+   static init(connection){
+       super.init({
+           data: DataTypes.STRING,
+           nome_anuncio:DataTypes.STRING
+       },{
+           sequelize:connection
+       })
+   }
+
+   static associate(models){
+       this.belongsTo(models.Usuario,{foreignKey:"id_usuario"});
+       this.belongsTo(models.Anuncio,{foreignKey:"id_anuncio"});
+   }
+
+
+
+}
+
+module.exports = Agenda;
